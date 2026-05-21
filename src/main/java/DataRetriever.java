41193373ae222134169a16b79ee45bcce363b27f
jessyrand <henintsoarandriam@gmail.com>
2026-05-21 11:41:21 +0300

    feat(DataRetriever): add findConsultantsByGrade method

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -68,4 +68,37 @@ public class DataRetriever {
             throw new RuntimeException(e);
         }
     }
+
+    public List<Consultant> findConsultantsByGrade(Grade grade) {
+        DBConnection dbConnection = new DBConnection();
+
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("""
+                select id, name, grade
+                from consultant
+                where grade = ?
+                """);
+
+            preparedStatement.setString(1, grade.name());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Consultant> consultants = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Consultant consultant = new Consultant();
+
+                consultant.setId(resultSet.getString("id"));
+                consultant.setName(resultSet.getString("name"));
+                consultant.setGrade(Grade.valueOf(resultSet.getString("grade")));
+
+                consultants.add(consultant);
+            }
+
+            return consultants;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
