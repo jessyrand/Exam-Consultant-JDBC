b5522e9b43ec0947a409d0206c3d03eea5af8043
jessyrand <henintsoarandriam@gmail.com>
2026-05-21 10:57:27 +0300

    feat(Mission): add constructor, getters and setters

--- src/main/java/Mission.java
+++ src/main/java/Mission.java
@@ -5,4 +5,44 @@ public class Mission {
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    public Mission(String id, String description, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
 }
