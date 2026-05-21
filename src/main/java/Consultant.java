08d69e1de70bc82769345548fbe67c267b007255
jessyrand <henintsoarandriam@gmail.com>
2026-05-21 10:56:47 +0300

    chore(Consultant): fix format

--- src/main/java/Consultant.java
+++ src/main/java/Consultant.java
@@ -8,22 +8,29 @@ public class Consultant {
         this.name = name;
         this.grade = grade;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public Grade getGrade() {
         return grade;
     }
+
     public void setGrade(Grade grade) {
         this.grade = grade;
     }
+
 }
